dd6ae75c0015719cfa2e4ae0888fd6c24cc69fb8
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2022-12-14 23:42:51 +0000

    Parent class completed

--- src/day38_OOPInheritanceCont_MethodOverriding/carTask/Car.java
+++ src/day38_OOPInheritanceCont_MethodOverriding/carTask/Car.java
@@ -7,10 +7,34 @@ public class Car {
     public String color;
     public int miles;
 
+    public Car(String brand, String model, int year, double price, String color, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
 
+    public void start() {
+        System.out.println(brand + model + " is starting");
+    }
+        public void drive () {
+            System.out.println(brand + model + " is driving");
+        }
 
-
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
 }
+
 
 /*
 carTask:
