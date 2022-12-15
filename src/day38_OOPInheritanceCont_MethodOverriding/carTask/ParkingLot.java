3feb169552b86094124fec9739df373eceef8721
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2022-12-15 00:34:12 +0000

    Parking Lot Has a relationship is complete

---
+++ src/day38_OOPInheritanceCont_MethodOverriding/carTask/ParkingLot.java
@@ -0,0 +1,9 @@
+package day38_OOPInheritanceCont_MethodOverriding.carTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyota = new Toyota("Camry", 2020, 20000, "silver", 124568);
+        Tesla tesla = new Tesla("Model S", 2022, 100000, "black", 3000);
+        BMW bmw = new BMW("S200", 2018,45000, "yellow", 45000);
+    }
+}
