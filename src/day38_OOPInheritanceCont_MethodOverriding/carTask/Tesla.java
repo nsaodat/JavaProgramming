0cd5defe56b41c53b74916f5b64609ed7307045f
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2022-12-15 00:05:56 +0000

    BMW and Tesla classes completed

---
+++ src/day38_OOPInheritanceCont_MethodOverriding/carTask/Tesla.java
@@ -0,0 +1,12 @@
+package day38_OOPInheritanceCont_MethodOverriding.carTask;
+
+public class Tesla extends Car{
+    public Tesla(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+
+    public void  autoPilot(){
+        System.out.println(brand +model + " is in autopilot mode");
+    }
+
+}
