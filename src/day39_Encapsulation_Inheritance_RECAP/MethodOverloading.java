d9f80fe7f678e0f6bf387389d297e41fe7506d09
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2022-12-16 13:28:57 +0000

    day39_working on Shapes Task

---
+++ src/day39_Encapsulation_Inheritance_RECAP/MethodOverloading.java
@@ -0,0 +1,14 @@
+package day39_Encapsulation_Inheritance_RECAP;
+
+public class MethodOverloading {
+
+    private void method1(){
+
+    }
+
+    private int method1(int a){
+        return a;
+    }
+
+
+}
---
+++ src/day39_Encapsulation_Inheritance_RECAP/ShapeTask/Shape.java
@@ -0,0 +1,47 @@
+package day39_Encapsulation_Inheritance_RECAP.ShapeTask;
+
+public class Shape {
+    private String name;// none of the Classes will have any access to this instance variable.
+                   // Developers can add additional requirements/conditions to getter and setter when the instance variable is private
+
+    public String getName() { // created getter and setter automatically
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Shape(String name) {  // I need a constructor, so as soon as the Object is created we can set the name
+      if (name == null){
+          System.err.println("Name can not be null");
+          System.exit(1);// status 1 --> something went wrong
+      }
+
+      if (name.isBlank()|| name.isBlank()){
+          System.err.println("Invalid name");
+          System.exit(1);
+      }
+
+        this.name = name;
+    }
+
+    public double area(){
+        return 0;
+    }
+
+    public double parameter(){
+        return 0;
+    }
+}
+
+/*
+	Shape:
+	variables:
+			name
+	Encapsulate the field
+	Add a constructor to set the filed
+	Methods:
+		area(){}
+		perimeter(){}
+ */
