c8dceff76e10ee1abca5ac51459f5c2e89a2234a
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2022-12-28 15:14:53 +0000

    day42_Exceptions continued_Throw2 keyword

--- src/day42_Exceptions_Continued/Throw_Keyword2.java
+++ src/day42_Exceptions_Continued/Throw_Keyword2.java
@@ -1,14 +1,24 @@
 package day42_Exceptions_Continued;
 
+import day39_Encapsulation_Inheritance_RECAP.ShapeTask.Square;
+
 import java.util.NoSuchElementException;
 
 public class Throw_Keyword2 {
     public static void main(String[] args) {
 
-        // we can not create a throw exception for checked exceptions.  compile will show an error --> throw new InterruptedException();
+         // throw new InterruptedException(); // --> we can not create a throw exception for checked exceptions.  compile will show an error
+
 
        throw new NoSuchElementException("No Such Element");// <-- unchecked exception is compatible with throw keyword for creating a manual exception.
 
-        // compiler will not print the code after the manual exception. Message wont print --> System.out.println("Hello World!");
+        // a class can have multiple constructors by method overloading:
+        // - default constructor --> throw new NoSuchElementException ();
+        // - constructor with a String message --> Parent class constructor takes the String argument as a message
+        // exceptions should have is a relationship with Throwable, otherwise we CAN NOT USE THROW keyword!
+
+        // compiler will not print the code after the manual exception. Message won`t print --> System.out.println("Hello World!");
+
+       // throw new Square()  -->  Square class is not throwable as it is not a child or grandchild of Throwable exception
     }
 }
