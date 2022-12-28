f595d8bb87ae4070978b573c86dd6cae0e77ae34
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2022-12-28 01:21:25 +0000

    day42_Exceptions continued_Throw keyword_manual creation of exception

--- src/day42_Exceptions_Continued/Throw_Keyword2.java
+++ src/day42_Exceptions_Continued/Throw_Keyword2.java
@@ -1,4 +1,14 @@
 package day42_Exceptions_Continued;
 
+import java.util.NoSuchElementException;
+
 public class Throw_Keyword2 {
+    public static void main(String[] args) {
+
+        // we can not create a throw exception for checked exceptions.  compile will show an error --> throw new InterruptedException();
+
+       throw new NoSuchElementException("No Such Element");// <-- unchecked exception is compatible with throw keyword for creating a manual exception.
+
+        // compiler will not print the code after the manual exception. Message wont print --> System.out.println("Hello World!");
+    }
 }
