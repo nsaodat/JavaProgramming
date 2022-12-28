5cf999740f212c0a93db6dd21ebf38b5852836e5
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2022-12-28 15:35:07 +0000

    day42_Exceptions continued_Custom Exceptions

--- src/day42_Exceptions_Continued/CustomException.java
+++ src/day42_Exceptions_Continued/CustomException.java
@@ -1,7 +1,38 @@
 package day42_Exceptions_Continued;
 
+class FadyException extends RuntimeException{ // custom unchecked exception
+
+
+    public FadyException(){
+        super("Time for a short break");
+    }
+
+    public FadyException(String message){
+        super(message);
+    }
+
+}
+
+
+class NoBreakException extends Exception{ // custom checked exception
+
+}
 
 public class CustomException {
 
+    public static void main(String[] args) {
+
+        // throw new FadyException("It's time for lunch break");
+
+
+        try {
+            throw new NoBreakException();
+        } catch (NoBreakException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 }
+
