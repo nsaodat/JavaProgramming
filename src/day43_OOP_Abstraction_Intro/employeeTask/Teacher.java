31d5e7d4eb2c463597343bc5da059375bb2b3c97
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2023-01-03 23:34:04 +0000

    day43_Abstraction Intro

--- src/day43_OOP_Abstraction_Intro/employeeTask/Teacher.java
+++ src/day43_OOP_Abstraction_Intro/employeeTask/Teacher.java
@@ -1,4 +1,17 @@
 package day43_OOP_Abstraction_Intro.employeeTask;
 
-public class Teacher {
+public class Teacher extends  Employee{
+    public Teacher(String name, int age, char gender, int id, String jobTitle, double salary) {
+        super(name, age, gender, id, jobTitle, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getName()+" is teaching.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println(getName()+" is slepping 8 hours.");
+    }
 }
