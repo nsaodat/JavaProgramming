2f02fa49e749156c2aebc0f1593f0e181d042d76
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2023-01-03 23:44:00 +0000

    Revert "day42_Exceptions continued_Throws keyword pros/cons"
    This reverts commit 17676d80

--- src/day43_OOP_Abstraction_Intro/employeeTask/Developer.java
+++ src/day43_OOP_Abstraction_Intro/employeeTask/Developer.java
@@ -1,6 +1,6 @@
 package day43_OOP_Abstraction_Intro.employeeTask;
 
-public class Developer extends Employee {
+public final class Developer extends Employee {
 
     public Developer(String name, int age, char gender, int id, String jobTitle, double salary) {
         super(name, age, gender, id, jobTitle, salary);
--- src/day43_OOP_Abstraction_Intro/employeeTask/Driver.java
+++ src/day43_OOP_Abstraction_Intro/employeeTask/Driver.java
@@ -1,6 +1,6 @@
 package day43_OOP_Abstraction_Intro.employeeTask;
 
-public class Driver extends Employee {
+public final class Driver extends Employee {
     public Driver(String name, int age, char gender, int id, String jobTitle, double salary) {
         super(name, age, gender, id, jobTitle, salary);
     }
