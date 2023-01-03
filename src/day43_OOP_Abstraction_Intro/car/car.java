1642a5a2137ed3bd461149d8ead8aa0c6658a85d
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2023-01-03 14:48:43 +0000

    day43_Abstraction Intro

---
+++ src/day43_OOP_Abstraction_Intro/car/car.java
@@ -0,0 +1,52 @@
+package day43_OOP_Abstraction_Intro.car;
+
+public class car {
+    private final String make, model;
+    private String color;
+    private final int year;
+    private double price;
+
+    public car(String make, String model, String color, int year, double price) {
+        this.make = make;
+        this.model = model;
+        setColor(color);
+
+        if (year < 1886){
+            throw new RuntimeException("Invalid Year: "+ year);
+        }
+        this.year = year;// create a condition that the right data is provided, otherwise the programme gets terminated. Condition --> line 14
+
+        if (price <= 0){ // we give the conditions before setting the year, so that it can throw an exception if requirements are not met!
+            throw new RuntimeException("Invalid price: " + price);
+        }
+        setPrice(price);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
