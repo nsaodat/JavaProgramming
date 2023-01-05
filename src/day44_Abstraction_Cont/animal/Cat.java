b3a75caf169c00fce6eef68f17bbe7abef55b8db
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2023-01-05 00:49:55 +0000

    day44_Animal Task

--- src/day44_Abstraction_Cont/animal/Cat.java
+++ src/day44_Abstraction_Cont/animal/Cat.java
@@ -1,6 +1,16 @@
 package day44_Abstraction_Cont.animal;
 
-public class Cat {
+import day44_Abstraction_Cont.Animal;
+
+public class Cat extends Animal {
+    public Cat(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + "eats Biryani");
+    }
 
 }
 
