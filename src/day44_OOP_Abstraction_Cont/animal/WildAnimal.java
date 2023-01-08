2ef5a4c8bcb30ea8f9dc91642ac8cf72f4dcc12a
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2023-01-08 19:47:55 +0000

    day45_Abstraction Cont...

--- src/day44_OOP_Abstraction_Cont/animal/WildAnimal.java
+++ src/day44_OOP_Abstraction_Cont/animal/WildAnimal.java
@@ -1,6 +1,6 @@
 package day44_OOP_Abstraction_Cont.animal;
 
-public interface WildAnimal { // Interface can be parent interface to other Classes
+public interface WildAnimal { // Supertype --> Interface can be parent interface to other Classes
 
     boolean isWild = true; // static & final by default
 
